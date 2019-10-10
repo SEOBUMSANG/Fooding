86cc868025ceace34d276454e341f4009e7bfe29
Jaehooooooon <jaehoon572@naver.com>
2019-10-10 15:35:41 +0900

    줌 인, 아웃 버튼 추가

--- app/src/main/java/com/example/fooding/SearchActivity.java
+++ app/src/main/java/com/example/fooding/SearchActivity.java
@@ -6,7 +6,9 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -17,7 +19,10 @@ import com.skt.Tmap.TMapCircle;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapView;
 
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+    TMapView tMapView;
     RelativeLayout layoutSearchButton;
 
     @Override
@@ -26,7 +31,7 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         LinearLayout layoutTmap = findViewById(R.id.layout_tmap);
-        TMapView tMapView = new TMapView(this);
+        tMapView = new TMapView(this);
         tMapView.setSKTMapApiKey("80e66504-97df-4d02-bc81-57c796cd67a1");   //API key setting
         layoutTmap.addView( tMapView );
         tMapView.setCenterPoint(126.963540, 37.509354, true);
@@ -41,17 +46,66 @@ public class SearchActivity extends AppCompatActivity {
         tMapCircle.setAreaAlpha(100);
         tMapView.addTMapCircle("circle1", tMapCircle);
 
+        //지도 이벤트 설정
+            // 클릭 이벤트 설정
+        tMapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
+            @Override
+            public boolean onPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                //Toast.makeText(MapEvent.this, "onPress~!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onPressUpEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                //Toast.makeText(MapEvent.this, "onPressUp~!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+            // 롱 클릭 이벤트 설정
+        tMapView.setOnLongClickListenerCallback(new TMapView.OnLongClickListenerCallback() {
+            @Override
+            public void onLongPressEvent(ArrayList arrayList, ArrayList arrayList1, TMapPoint tMapPoint) {
+                //Toast.makeText(MapEvent.this, "onLongPress~!", Toast.LENGTH_SHORT).show();
+            }
+        });
+            // 지도 스크롤 종료
+        tMapView.setOnDisableScrollWithZoomLevelListener(new TMapView.OnDisableScrollWithZoomLevelCallback() {
+            @Override
+            public void onDisableScrollWithZoomLevelEvent(float zoom, TMapPoint centerPoint) {
+                //Toast.makeText(getApplicationContext(), "zoomLevel=" + zoom + "\nlon=" + centerPoint.getLongitude() + "\nlat=" + centerPoint.getLatitude(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         //화면 설정
         Button settingButton = findViewById(R.id.setting_button);
         Button mylocationButton = findViewById(R.id.my_location_button);
         EditText locationInput = findViewById(R.id.location_input);
         Button locationInputButton = findViewById(R.id.location_input_button);
 
-        layoutSearchButton = findViewById(R.id.layout_search_button);
+        Button buttonZoomIn = findViewById(R.id.button_zoom_in);
+        Button buttonZoomOut = findViewById(R.id.button_zoom_out);
+
+        //layoutSearchButton = findViewById(R.id.layout_search_button);
         Button searchButton = findViewById(R.id.search_button);
         Button youtuberButton = findViewById(R.id.youtuber_button);
         Button worldcupButton = findViewById(R.id.worldcup_button);
         Button likeButton = findViewById(R.id.like_button);
+
+        // "확대" 버튼 클릭
+        buttonZoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tMapView.MapZoomIn();
+            }
+        });
+
+        // "축소" 버튼 클릭
+        buttonZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tMapView.MapZoomOut();
+            }
+        });
 
     }
 
@@ -61,4 +115,6 @@ public class SearchActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), "search", Toast.LENGTH_SHORT).show();
     }
+
+
 }
