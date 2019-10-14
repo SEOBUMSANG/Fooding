9967572f2969523607e00fa047d92b6087bad21f
CJBJ1 <cbjx17@naver.com>
2019-10-14 14:57:50 +0900

    searchVideo 함수 추가

--- functions/index.js
+++ functions/index.js
@@ -3,40 +3,45 @@ const functions = require('firebase-functions');
 const Youtube = require('youtube-node');
 admin.initializeApp(functions.config().firebase);
 
-exports.testFunction = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  let db = admin.firestore();
-  const original = req.query.text;
-  var youtube = new Youtube();
-  var word = "강남 맛집";
-  var limit = 10;
-  var snapshot;
-  youtube.setKey('AIzaSyBa6lDf0K-_meL_ON8kA4bEPIb3GctemO8');
+exports.searchVideo = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
 
+  let db = admin.firestore();
+
+  var youtube = new Youtube();
+  var wordArray = [];
+  var limit = 9;
+  var wordIndex = 0;
+  var count = 0;
+
+  youtube.setKey('AIzaSyBa6lDf0K-_meL_ON8kA4bEPIb3GctemO8');
   youtube.addParam('order', 'rating');
   youtube.addParam('type', 'video');
-  youtube.addParam('videoLicense', 'creativeCommon');
-  youtube.search(word, limit, function (err, result) {
+  youtube.addParam('videoLicense', 'youtube');
+
+
+  for (wordIndex in wordArray) {
+    youtube.search(wordArray[wordIndex], limit, function (err, result) {
+
       if (err) {
-          console.log(err);
-          return;
+        console.log(err);
+        return;
       }
 
       var items = result["items"];
-      for (var i in items) {
-          var it = items[i];
-          var title = it["snippet"]["title"];
-          var video_id = it["id"]["videoId"];
-          var thumbnail = it["snippet"]["thumbnails"]["default"]["url"];
-          var url = "https://www.youtube.com/watch?v=" + video_id;
-          let docRef = db.collection('users').doc(title);
-          let setAda = docRef.set({
-            title: title,
-            thumbnail: thumbnail,
-            URL: url
-          });
-          
-      }
-  });
 
+      for (var itemIndex in items) {
+        var it = items[itemIndex];
+        var title = it["snippet"]["title"];
+        var video_id = it["id"]["videoId"];
+        var thumbnail = it["snippet"]["thumbnails"]["default"]["url"];
+        var url = "https://www.youtube.com/watch?v=" + video_id;
+        let docRef = db.collection(wordArray[count]).doc('영상 ' + itemIndex).set({
+          title: title,
+          thumbnail: thumbnail,
+          URL: url
+        });
+      }
+      count++;
+    });
+  }
 });
