023200ff4b03a96980456bbe83cc3d765632c0f3
CJBJ1 <cbjx17@naver.com>
2019-11-02 20:15:10 +0900

    일정거리 이내의 마커만 선택

--- app/src/main/java/com/example/fooding/SearchDB.java
+++ app/src/main/java/com/example/fooding/SearchDB.java
@@ -1,5 +1,6 @@
 package com.example.fooding;
 
+import android.location.Location;
 import android.util.Log;
 
 import com.google.android.gms.tasks.Continuation;
@@ -11,6 +12,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.skt.Tmap.TMapPoint;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,7 +27,7 @@ public class SearchDB {
     public SearchDB() {
     }
 
-    public void returnData(final ArrayList<JSONObject> jsonObjectArrayList) {
+    public void returnData(final ArrayList<JSONObject> jsonObjectArrayList,final TMapPoint centerPoint) {
         db.collection("Crawling")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -33,9 +35,15 @@ public class SearchDB {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                JSONObject jsonObject = new JSONObject();
-                                Log.d("TAG", document.getId() + " => " + document.getData().get("name"));
-                                returnYoutube(jsonObjectArrayList,jsonObject,document.getId(),document.getData().get("name"),document.getData().get("lat"),document.getData().get("lng"));
+                                double distance = checkDistance(document,centerPoint);
+                                if(distance>1000) {
+                                    continue;
+                                }
+                                else {
+                                    JSONObject jsonObject = new JSONObject();
+                                    Log.d("TAG", document.getId() + " => " + document.getData().get("name"));
+                                    returnYoutube(jsonObjectArrayList, jsonObject, document.getId(), document.getData().get("name"), document.getData().get("lat"), document.getData().get("lng"));
+                                }
                             }
                         } else {
                             Log.d("TAG", "Error getting documents: ", task.getException());
@@ -83,5 +91,29 @@ public class SearchDB {
                     }
                 });
     }
+
+    public double checkDistance(QueryDocumentSnapshot document, TMapPoint point){
+        Location locationA = new Location("point A");
+        Location locationB = new Location("point B");
+
+        locationA.setLatitude(point.getLatitude());
+        locationA.setLongitude(point.getLongitude());
+
+        Log.d("개씨빨",Double.toString(locationA.getLatitude()));
+        Log.d("개씨빨",Double.toString(locationA.getLongitude()));
+
+
+        locationB.setLatitude(Double.parseDouble(document.getData().get("lat").toString()));
+        locationB.setLongitude(Double.parseDouble(document.getData().get("lng").toString()));
+
+        Log.d("개씨빨",Double.toString(locationB.getLatitude()));
+        Log.d("개씨빨",Double.toString(locationB.getLongitude()));
+
+        double distance = locationA.distanceTo(locationB);
+
+        Log.d("개씨빨",Double.toString(distance));
+        return distance;
+    }
+
 
 }
