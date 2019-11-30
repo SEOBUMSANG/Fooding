b735fcf94d960a95822a2a19939773c45bd4c479
CJBJ1 <cbjx17@naver.com>
2019-11-30 16:01:45 +0900

    Iterator 사용

--- app/src/main/java/com/example/fooding/Search2Activity.java
+++ app/src/main/java/com/example/fooding/Search2Activity.java
@@ -531,7 +531,9 @@ public class Search2Activity extends MapActivity {
     }
 
     public void showMarker(ArrayList<TMapMarkerItem> markerList, TMapPoint centerPoint) {
-        for (TMapMarkerItem marker : markerList) {
+        Iterator<TMapMarkerItem> iter = markerList.iterator();
+        while (iter.hasNext()) {
+            TMapMarkerItem marker = iter.next();
             Location.distanceBetween(marker.latitude, marker.longitude, centerPoint.getLatitude(), centerPoint.getLongitude(), dist);
 
             if (bigMode) {
@@ -539,7 +541,7 @@ public class Search2Activity extends MapActivity {
                     continue;
                 tMapView.addMarkerItem(marker.getID(), marker);    // 지도에 추가
             } else {
-                if(dist[0]>500)
+                if (dist[0] > 500)
                     continue;
                 tMapView.addMarkerItem(marker.getID(), marker);    // 지도에 추가
             }
@@ -548,7 +550,9 @@ public class Search2Activity extends MapActivity {
 
 
     public void showMarker2(ArrayList<TMapMarkerItem2> markerList, TMapPoint centerPoint) {
-        for (TMapMarkerItem2 marker : markerList) {
+        Iterator<TMapMarkerItem2> iter = markerList.iterator();
+        while (iter.hasNext()) {
+            TMapMarkerItem2 marker = iter.next();
             Location.distanceBetween(marker.latitude, marker.longitude, centerPoint.getLatitude(), centerPoint.getLongitude(), dist);
 
             if (bigMode) {
@@ -556,7 +560,7 @@ public class Search2Activity extends MapActivity {
                     continue;
                 tMapView.addMarkerItem2(marker.getID(), marker);    // 지도에 추가
             } else {
-                if(dist[0]>500)
+                if (dist[0] > 500)
                     continue;
                 tMapView.addMarkerItem2(marker.getID(), marker);    // 지도에 추가
             }
